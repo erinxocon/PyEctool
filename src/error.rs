d70dcc958ddeb408b5315a3097a913fe504068c4
Erin O'Connell <erinocon5@gmail.com>
2025-07-08 00:00:58 -0600

    (*) error now is a bit more idomatic for rust i think!

--- src/error.rs
+++ src/error.rs
@@ -1,3 +1,26 @@
+use std::fmt;
+
+use pyo3::prelude::*;
+use pyo3::exceptions::PyValueError;
+
+#[derive(Debug)]
 pub enum Error {
-    UnknownLedMode,
+    UnknownLedMode(u8),
 }
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::UnknownLedMode(value) => write!(f,"Unknown Led Mode: {value}"),
+        }
+    }
+}
+
+impl From<Error> for PyErr {
+    fn from(err: Error) -> PyErr {
+        match err {
+            Error::UnknownLedMode(_) => PyValueError::new_err(err.to_string()),
+        }
+    }
+}
+
