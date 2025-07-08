7d61868d88b28ff9a952c417107db90a92b3d76b
Erin O'Connell <erinocon5@gmail.com>
2025-07-07 23:59:07 -0600

    (*) Remove fluff from the main Ec class, strictly mirror rust counterpart

--- src/ec.rs
+++ src/ec.rs
@@ -1,40 +1,48 @@
+use std::fmt;
+use std::string;
+
 use pyo3::prelude::*;
-use pyo3::exceptions::PyRuntimeError;
+use pyo3::exceptions::{PyRuntimeError, PyUnicodeDecodeError};
 use hidapi::HidApi;
 
-use ectool::{Ec, Error, Access, AccessHid};
+use ectool::{Ec, Error as EcError, Access, AccessHid};
 
-fn to_py_err(err: Error) -> PyErr {
+fn ecerror_to_py_err(err: EcError) -> PyErr {
     PyRuntimeError::new_err(format!("EC error: {err:?}"))
 }
 
-fn to_py_hid(err: hidapi::HidError) -> PyErr {
-    PyRuntimeError::new_err(format!("hidapi: {err}"))
+fn hid_to_py_err(err: hidapi::HidError) -> PyErr {
+    PyRuntimeError::new_err(format!("hidapi: {err:?}"))
 }
 
-#[pyclass(unsendable)]
-pub struct EcController {
+fn string_to_py_err(err: string::FromUtf8Error) -> PyErr {
+    PyUnicodeDecodeError::new_err(format!("{err:?}"))
+}
+
+#[pyclass(unsendable, str)]
+pub struct PyEc {
     ec: Ec<Box<dyn Access>>,
     #[pyo3(get)]
     led_map: Vec<Vec<u8>>,
     #[pyo3(get)]
-    framebuffer: FrameBuffer,
-    saved_layer_mode: Option<u8>,
+    board: String,
+    #[pyo3(get)]
+    version: String,
 }
 
 #[pymethods]
-impl EcController {
+impl PyEc {
     #[new]
     pub fn new() -> PyResult<Self> {
         let ni = 255;
-        let api = HidApi::new().map_err(to_py_hid)?;
+        let api = HidApi::new().map_err(hid_to_py_err)?;
         for info in api.device_list() {
             match (info.vendor_id(), info.product_id(), info.interface_number()) {
                 // System76 Launch keyboards
                 (0x3384, 0x0001..=0x000A, 1) => {
-                    let device = info.open_device(&api).map_err(to_py_hid)?;
-                    let access = AccessHid::new(device, 10, 100).map_err(to_py_err)?;
-                    let ec = unsafe { Ec::new(access).map_err(to_py_err)? }.into_dyn();
+                    let device = info.open_device(&api).map_err(hid_to_py_err)?;
+                    let access = AccessHid::new(device, 10, 100).map_err(ecerror_to_py_err)?;
+                    let mut ec = unsafe { Ec::new(access).map_err(ecerror_to_py_err)? }.into_dyn();
 
                     //refactor this to set these per keyboard layout based on device info
                     let led_map = vec![
@@ -45,8 +53,25 @@ impl EcController {
                         vec![12, 11, 10,  9,  8,  7,  6,  5,  4,  3,  2,  1,  0, ni, ni],
                         vec![13, 14, 15, 16, 17, ni, 18, 19, 20, 21, ni, 22, 23, 24, ni],
                     ];
-                    let framebuffer = FrameBuffer::new(led_map.clone());
-                    return Ok(Self { ec, led_map, framebuffer, saved_layer_mode: None });
+
+                    let data_size = unsafe { ec.access().data_size() };
+                    let mut data = vec![0; data_size];
+
+
+                    let board = {
+                        let size = unsafe { ec.board(&mut data).map_err(ecerror_to_py_err)? };
+                        data.truncate(size);
+                        String::from_utf8(data.clone()).map_err(string_to_py_err)?
+                    };
+
+                    let version = {
+                        let size = unsafe { ec.version(&mut data).map_err(ecerror_to_py_err)? };
+                        data.truncate(size);
+                        String::from_utf8(data).map_err(string_to_py_err)?
+                    };
+
+
+                    return Ok(Self { ec, led_map, board, version });
                 }
                 _ => {}
             }
@@ -55,81 +80,37 @@ impl EcController {
         Err(PyRuntimeError::new_err("No compatible EC HID device found"))
     }
 
-    pub fn open(&mut self) -> PyResult<()> {
-        unsafe { self.ec.led_set_mode(0, LedModes::PER_KEY, 0).map_err(to_py_err)? };
-        Ok(())
-    }
-
-    pub fn close(&mut self) -> PyResult<()> {
-        unsafe { self.ec.led_set_mode(0, LedModes::LAST, 75).map_err(to_py_err)? }
-        Ok(())
-    }
-
-    pub fn __enter__(&mut self) -> PyResult<Self> {
-        self.open()?;
-        Ok(self)
-    }
-
-    pub fn __exit__(
-        &mut self,
-        _exc_type: Option<Py<PyAny>>,
-        _exc_value: Option<Py<PyAny>>,
-        _traceback: Option<Py<PyAny>>,
-    ) -> PyResult<bool> {
-        self.close()?;
-        Ok(false)
-    }
-
-    #[getter]
-    fn board(&mut self) -> PyResult<String> {
-        ec_board(&mut self.ec).map_err(to_py_err)
-    }
-
-    #[getter]
-    fn version(&mut self) -> PyResult<String> {
-        ec_version(&mut self.ec).map_err(to_py_err)
-    }
-
     pub fn led_get_value(&mut self, index: u8) -> PyResult<(u8, u8)> {
-        unsafe { self.ec.led_get_value(index).map_err(to_py_err) }
+        unsafe { self.ec.led_get_value(index).map_err(ecerror_to_py_err) }
     }
 
     pub fn led_set_value(&mut self, index: u8, value: u8) -> PyResult<()> {
-        unsafe { self.ec.led_set_value(index, value).map_err(to_py_err) }
+        unsafe { self.ec.led_set_value(index, value).map_err(ecerror_to_py_err) }
     }
 
     pub fn led_get_mode(&mut self, layer: u8) -> PyResult<(u8, u8)> {
-        unsafe { self.ec.led_get_mode(layer).map_err(to_py_err) }
+        unsafe { self.ec.led_get_mode(layer).map_err(ecerror_to_py_err) }
     }
 
     pub fn led_set_mode(&mut self, layer: u8, mode: u8, speed: u8) -> PyResult<()> {
-        unsafe { self.ec.led_set_mode(layer, mode, speed).map_err(to_py_err) }
+        unsafe { self.ec.led_set_mode(layer, mode, speed).map_err(ecerror_to_py_err) }
     }
 
     pub fn led_get_color(&mut self, index: u8) -> PyResult<(u8, u8, u8)> {
-        unsafe { self.ec.led_get_color(index).map_err(to_py_err) }
+        unsafe { self.ec.led_get_color(index).map_err(ecerror_to_py_err) }
     }
 
     pub fn led_set_color(&mut self, index: u8, r: u8, g: u8, b: u8) -> PyResult<()> {
-        unsafe { self.ec.led_set_color(index, r, g, b).map_err(to_py_err) }
+        unsafe { self.ec.led_set_color(index, r, g, b).map_err(ecerror_to_py_err) }
     }
 
-    pub fn get_led(&mut self, index: u8) -> PyResult<Led> {
-        let (r, g, b) = unsafe { self.ec.led_get_color(index).map_err(to_py_err)? };
-        Ok(Led {
-            index,
-            color: (r, g, b),
-            sync_color: None,
-        })
+    fn __repr__(&self) -> String {
+        format!("<PyEc board={} version={}>", self.board, self.version)
     }
+}
 
-    pub fn set_led(&mut self, mut led: Led) -> PyResult<()> {
-        led.sync(&mut self.ec).map_err(to_py_err)?;
-        Ok(())
-    }
-
-    pub fn render_framebuffer(&mut self) -> PyResult<()> {
-        self.framebuffer.render(&mut self.ec).map_err(to_py_err)?;
-        Ok(())
+impl fmt::Display for PyEc {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "System76 EC [{} / {}]", self.board, self.version)
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,18 @@
-// pub use self::ec::EcController;
-// mod ec;
+pub use self::ec::PyEc;
+mod ec;
 
-pub use self::led::LedMode;
-mod led;
+// pub use self::led::LedMode;
+// mod led;
 
-pub use self::error::Error;
-mod error;
+// pub use self::error::Error;
+// mod error;
 
 use pyo3::prelude::*;
 
 /// A Python module implemented in Rust.
 #[pymodule]
 fn pyectool(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    //m.add_class::<crate::ec::EcController>()?;
-    m.add_class::<crate::led::LedMode>()?;
+    m.add_class::<crate::ec::PyEc>()?;
+    // m.add_class::<crate::led::LedMode>()?;
     Ok(())
 }
