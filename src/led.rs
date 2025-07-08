dcf096109daacab21776c249e84676e3a142a4cb
Erin O'Connell <erinocon5@gmail.com>
2025-07-08 00:03:44 -0600

    (*) add Led helper class that implements  since sync logic

--- src/led.rs
+++ src/led.rs
@@ -1,11 +1,11 @@
-use pyo3::prelude::*;
+use pyo3::{prelude::*, types::PyType};
 
 use std::fmt;
-use crate::{Error};
+use crate::{Error as PyEcErr};
 
 
 #[pyclass(eq, eq_int, rename_all = "SCREAMING_SNAKE_CASE", str)]
-#[derive(PartialEq, Debug)]
+#[derive(PartialEq, Debug, Copy, Clone)]
 #[repr(u8)]
 pub enum LedMode {
     SolidColor = 0,
@@ -27,7 +27,7 @@ pub enum LedMode {
 }
 
 impl TryFrom<u8> for LedMode {
-    type Error = Error;
+    type Error = PyEcErr;
 
     fn try_from(value: u8) -> Result<Self, Self::Error> {
         match value {
@@ -47,7 +47,7 @@ impl TryFrom<u8> for LedMode {
             13 => Ok(Self::ActiveKeys),
             14 => Ok(Self::Disabled),
             15 => Ok(Self::Last),
-            _ => Err(Error::UnknownLedMode),
+            other => Err(PyEcErr::UnknownLedMode(other)),
         }
     }
 }
@@ -55,8 +55,8 @@ impl TryFrom<u8> for LedMode {
 #[pymethods]
 impl LedMode {
     #[classmethod]
-    pub fn from_int(_cls: &PyType, value: u8) -> PyResult<Self> {
-        Self::try_from(value).map_err(|e| PyErr::new::<pyo3::exceptions::PyValueError, _>(format!("{e}")))
+    pub fn from_int(_cls: Bound<'_, PyType>, value: u8) -> PyResult<Self> {
+        Ok(Self::try_from(value)?)
     }
 }
 
@@ -80,5 +80,70 @@ impl fmt::Display for LedMode {
             Self::Disabled => write!(f, "Disabled"),
             Self::Last => write!(f, "Last"),
         }
+    }
+}
+
+fn hex_to_rgb(hex: u32) -> (u8, u8, u8) {
+    let r = ((hex >> 16) & 0xFF) as u8;
+    let g = ((hex >> 8) & 0xFF) as u8;
+    let b = (hex & 0xFF) as u8;
+    (r, g, b)
+}
+
+#[pyclass(str)]
+#[derive(Debug)]
+struct Led {
+    #[pyo3(get)]
+    index: u8,
+    #[pyo3(get)]
+    color: (u8, u8, u8),
+    #[pyo3(get)]
+    sync_color: (u8, u8, u8),
+}
+
+#[pymethods]
+impl Led {
+    #[new]
+    pub fn new(index: u8, r: u8, g: u8, b: u8) -> Self {
+        Self {index, color: (r, g, b), sync_color: (r, g, b)}
+    }
+
+    #[classmethod]
+    pub fn from_rgb(_cls: Bound<'_, PyType>, index: u8, color: (u8, u8, u8)) -> Self {
+        Self { index, color, sync_color: color}
+    }
+
+    #[classmethod]
+    pub fn from_hex(_cls: Bound<'_, PyType>, index: u8, hex: u32) -> Self {
+        let color = hex_to_rgb(hex);
+        Self {
+            index,
+            color,
+            sync_color: color,
+        }
+    }
+
+    pub fn set_color_rgb(&mut self, r: u8, g:u8, b: u8) -> PyResult<()> {
+        self.sync_color = (r, g, b);
+        Ok(())
+    }
+
+    pub fn set_color_hex(&mut self, hex: u32) -> PyResult<()> {
+        self.sync_color = hex_to_rgb(hex);
+        Ok(())
+    }
+}
+
+
+impl fmt::Display for Led {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f, 
+            "Red: {}, Green: {}, Blue: {}. Sync Status: {}.", 
+            self.color.0,
+            self.color.1,
+            self.color.2,
+            (self.color == self.sync_color)
+        )
     }
 }
