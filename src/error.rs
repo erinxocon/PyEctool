995c4ceed7b68a7e593a41e5b228947b2c2259f2
Erin O'Connell <erinocon5@gmail.com>
2025-07-13 01:16:18 -0600

    (+) added new ColorToLarge error to enforce 24bit color

--- src/error.rs
+++ src/error.rs
@@ -6,12 +6,14 @@ use pyo3::exceptions::PyValueError;
 #[derive(Debug)]
 pub enum PyEcErr {
     UnknownLedMode(u8),
+    ColorToLarge(u32),
 }
 
 impl fmt::Display for PyEcErr {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            Self::UnknownLedMode(value) => write!(f,"Unknown Led Mode: {value}"),
+            Self::UnknownLedMode(mode) => write!(f,"Unknown Led Mode: {}", mode),
+            Self::ColorToLarge(hex) => write!(f, "Color value must be a 24-bit RGB Color {} > 0xFFFFFF", hex)
         }
     }
 }
@@ -19,8 +21,7 @@ impl fmt::Display for PyEcErr {
 impl From<PyEcErr> for PyErr {
     fn from(err: PyEcErr) -> PyErr {
         match err {
-            PyEcErr::UnknownLedMode(_) => PyValueError::new_err(err.to_string()),
+            PyEcErr::UnknownLedMode(_) | PyEcErr::ColorToLarge(_) => PyValueError::new_err(err.to_string())
         }
     }
 }
-
